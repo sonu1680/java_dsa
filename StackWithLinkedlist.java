12217f865e582fe8a4f60de0a927e7aa1adab8ca
sonu1680 <sonupandit1680@gmail.com>
2025-06-20 15:01:17 +0530

    stackWithLinkedist completd

--- StackWithLinkedlist.java
+++ StackWithLinkedlist.java
@@ -1,17 +1,67 @@
+
 //stack with linkedlist
 import java.util.LinkedList;
 
 public class StackWithLinkedlist {
-    
 
-public class Stack{
+    static class Stack {
 
-LinkedList<Integer>ll=new LinkedList<>();
+        static class Node {
+            int data;
+            Node next;
 
-public static boolean isEmpty(){
-if(ll.head)
+            Node(int data) {
+                this.data = data;
+                this.next = null;
+            }
+
+        }
+
+        static Node head = null;
+        static Node tail = null;
+
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        public static void push(int data) {
+            Node newNode = new Node(data);
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+
+        }
+
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int data=head.data;
+            head=head.next;
+            return data;
+
+        }
+
+        public static int peek(){
+if(isEmpty()){
+    return -1;
 }
+return head.data;
+        }
+    }
+public static void main(String args[]){
+    Stack s=new Stack();
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+while(!s.isEmpty()){
 
+     System.out.println(s.peek());
+     s.pop();
 }
-
+}
 }
